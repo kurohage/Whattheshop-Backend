cf7c932d4ad38bab185adcf47c7f9c36234c6568
kurohage <517326+kurohage@users.noreply.github.com>
2019-12-03 20:11:49 +0300

    created permissions to view orders for logged in user only

--- api/permissions.py
+++ api/permissions.py
@@ -1,1 +1,10 @@
 from rest_framework.permissions import BasePermission
+
+class IsOrderOwner(BasePermission):
+	message = "You must be the owner of this order"
+
+	def has_object_permission(self, request, view, object):
+		if request.user.is_staff or (object.profile.user == request.user):
+			return True
+		else:
+			return False
